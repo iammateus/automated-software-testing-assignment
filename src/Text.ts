080a2bfc502cd9015c4a49674f07d84e2bd794cf
Mateus Soares <mateussoaress1997@gmail.com>
2021-09-23 15:05:44 -0300

    Add true cases for isPalindrome

--- src/Text.ts
+++ src/Text.ts
@@ -1,7 +1,27 @@
 export default class Text {
-    text: string;
+    body: string;
 
-    constructor(text: string) {
-        this.text = text;
+    constructor(body: string) {
+        this.body = body;
+    }
+
+    isPalindrome(): boolean {
+        const clearBody = Text.clean(this.body);
+
+        const middle = Math.floor(clearBody.length / 2);
+        const remainder = clearBody.length % 2;
+
+        const firstHalf = clearBody.substr(0, middle);
+        const secondHalf = clearBody.substr(middle + remainder, middle);
+        const invertedSecondHalf = secondHalf.split('').reverse().join('');
+
+        console.log({ firstHalf, invertedSecondHalf });
+        return firstHalf === invertedSecondHalf;
+    }
+
+    private static clean(text: string): string {
+        let result: string = text.toLowerCase();
+        result = result.replace(/[^0-9a-z]/gi, '');
+        return result;
     }
 }
--- tests/Text.test.ts
+++ tests/Text.test.ts
@@ -12,3 +12,60 @@ describe('Text', () => {
         expect(text).toBeInstanceOf(Text);
     });
 });
+
+describe('Text.isPalindrome', () => {
+    it('should exist as a function', () => {
+        const text: Text = new Text(faker.lorem.word());
+        expect(text.isPalindrome).toBeInstanceOf(Function);
+    });
+
+    it('should return true when text is palindrome (“Rotator”)', () => {
+        const text = new Text('Rotator');
+        expect(text.isPalindrome()).toBe(true);
+    });
+
+    it('should return true when text is palindrome (bob)', () => {
+        const text = new Text('bob');
+        expect(text.isPalindrome()).toBe(true);
+    });
+
+    it('should return true when text is palindrome (madam)', () => {
+        const text = new Text('madam');
+        expect(text.isPalindrome()).toBe(true);
+    });
+
+    it('should return true when text is palindrome (mAlAyAlam)', () => {
+        const text = new Text('mAlAyAlam');
+        expect(text.isPalindrome()).toBe(true);
+    });
+
+    it('should return true when text is palindrome (1)', () => {
+        const text = new Text('1');
+        expect(text.isPalindrome()).toBe(true);
+    });
+
+    it('should return true when text is palindrome (Able was I, ere I saw Elba)', () => {
+        const text = new Text('Able was I, ere I saw Elba');
+        expect(text.isPalindrome()).toBe(true);
+    });
+
+    it('should return true when text is palindrome (Madam I’m Adam)', () => {
+        const text = new Text('Madam I’m Adam');
+        expect(text.isPalindrome()).toBe(true);
+    });
+
+    it('should return true when text is palindrome (Step on no pets.)', () => {
+        const text = new Text('Step on no pets.');
+        expect(text.isPalindrome()).toBe(true);
+    });
+
+    it('should return true when text is palindrome (Top spot!)', () => {
+        const text = new Text('Top spot!');
+        expect(text.isPalindrome()).toBe(true);
+    });
+
+    it('should return true when text is palindrome (02/02/2020!)', () => {
+        const text = new Text('02/02/2020');
+        expect(text.isPalindrome()).toBe(true);
+    });
+});
